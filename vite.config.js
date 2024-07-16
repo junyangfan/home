4b271c74aaefff6a5ce7e548db96bca76d86f5bb
fanjunyang <fanjunyang@daojia-inc.com>
2024-07-16 19:39:36 +0800

    fix: 修改favicon

--- vite.config.js
+++ vite.config.js
@@ -52,37 +52,37 @@ export default ({ mode }) =>
           background_color: "#424242",
           icons: [
             {
-              src: "https://static.fjy.zone/images/favicon.png?imageMogr2/format/webp",
+              src: "https://assets.fjy.zone/images/favicon.png",
               sizes: "48x48",
               type: "image/png",
             },
             {
-              src: "https://static.fjy.zone/images/favicon.png?imageMogr2/format/webp",
+              src: "https://assets.fjy.zone/images/favicon.png",
               sizes: "72x72",
               type: "image/png",
             },
             {
-              src: "https://static.fjy.zone/images/favicon.png?imageMogr2/format/webp",
+              src: "https://assets.fjy.zone/images/favicon.png",
               sizes: "96x96",
               type: "image/png",
             },
             {
-              src: "https://static.fjy.zone/images/favicon.png?imageMogr2/format/webp",
+              src: "https://assets.fjy.zone/images/favicon.png",
               sizes: "128x128",
               type: "image/png",
             },
             {
-              src: "https://static.fjy.zone/images/favicon.png?imageMogr2/format/webp",
+              src: "https://assets.fjy.zone/images/favicon.png",
               sizes: "144x144",
               type: "image/png",
             },
             {
-              src: "https://static.fjy.zone/images/favicon.png?imageMogr2/format/webp",
+              src: "https://assets.fjy.zone/images/favicon.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "https://static.fjy.zone/images/favicon.png?imageMogr2/format/webp",
+              src: "https://assets.fjy.zone/images/favicon.png",
               sizes: "512x512",
               type: "image/png",
             },
