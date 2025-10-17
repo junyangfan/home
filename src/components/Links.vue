eac571f38877bac3926d6578287cf5ce3c3bccd2
fanjunyang <fanjunyang@daojia-inc.com>
2025-10-17 12:50:24 +0800

    feat: 修改

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Bomb, Water, ChartLine, Satellite, ShoppingBag, CompassRegular } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Bomb, Water, PlaneDeparture, ChartLine, Satellite, ShoppingBag, CompassRegular } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -75,7 +75,8 @@ const siteIcon = {
   Water,
   ChartLine,
   Satellite,
-  CompassRegular
+  CompassRegular,
+  PlaneDeparture
 };
 
 // 链接跳转
